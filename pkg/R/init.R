b4c0eeffc2a3e84084415b1770a64264d1de256a
cbeleites <cbeleites@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-18 11:16:05 +0000

    fresh build
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/arrayhelpers@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/init.R
@@ -0,0 +1,17 @@
+## .onLoad <- function (libname, pkgname){
+##   stopifnot (require (arrayhelpers))
+## }
+
+.onAttach <- function (libname, pkgname){
+  ## unlockBinding(".options", asNamespace("arrayhelpers")) 
+
+  desc <- utils::packageDescription("arrayhelpers")
+  vers <- paste("V. ", desc$Version)
+
+  cat ("Package ",  desc$Package, ", version ", desc$Version, "\n\n",
+       "If you use this package please cite it appropriately.\n",
+       "   citation(\"", desc$Package, "\")\nwill give you the correct reference.", "\n\n",
+       "The project homepage is ", desc$URL, "\n\n",
+       sep = "")
+}
+
