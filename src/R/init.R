28f6fe1873193ab020d9f6af46c7771cb0cf2671
cbeleites <cbeleites@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-29 14:12:32 +0000

    packageStartupMessage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/arrayhelpers@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/R/init.R
+++ src/R/init.R
@@ -8,7 +8,7 @@
   desc <- utils::packageDescription("arrayhelpers")
   vers <- paste("V. ", desc$Version)
 
-  cat ("Package ",  desc$Package, ", version ", desc$Version, "\n\n",
+  packageStartupMessage ("Package ",  desc$Package, ", version ", desc$Version, "\n\n",
        "If you use this package please cite it appropriately.\n",
        "   citation(\"", desc$Package, "\")\nwill give you the correct reference.", "\n\n",
        "The project homepage is ", desc$URL, "\n\n",
