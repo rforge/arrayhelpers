4357b34a316e6c7dfaa5c167577132a05aabb2fd
cbeleites <cbeleites@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-20 10:39:08 +0000

    *** empty log message ***
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/arrayhelpers@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/R/validate.R
+++ src/R/validate.R
@@ -7,19 +7,22 @@
 ##' @seealso  \link[svUnit]{svUnit} 
 ##' @export 
 arrayhelpers.unittest <- function (){
-  if (! require (svUnit, quietly = TRUE)){
+  if (! require ("svUnit", quietly = TRUE)){
     warning ("svUnit required to run the unit tests.")
     return (NA)
   }
 
   tests <- unlist (eapply (env = getNamespace ("arrayhelpers"), FUN = is.test, all.names = TRUE))
   tests <- names (tests [tests])
-
   tests <- sapply (tests, get, envir = getNamespace ("arrayhelpers"))
 
   clearLog ()
+  warnlevel <- options()$warn
+  options (warn = 0)
   for (t in seq_along (tests))
     runTest (tests [[t]], names (tests) [t])
+
+  options (warn = warnlevel)
   print (stats (Log()))
 
   errorLog (summarize = FALSE)
